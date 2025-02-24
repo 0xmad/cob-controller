77023c0aeacd853a92afd4ebcb1bc36a0ef01a46
Anton <14254374+0xmad@users.noreply.github.com>
2025-02-23 19:41:31 -0600

    feat: add message queue consumer

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,12 +3,26 @@ pub struct RedisSettings {
     pub pool_size: u32,
 }
 
+pub struct RabbitMQSettings {
+    pub url: String,
+    pub queue: String,
+    pub consumer_tag: String,
+}
+
 pub fn redis_settings() -> RedisSettings {
     RedisSettings {
         url: get_env("REDIS_URL"),
         pool_size: get_env("POOL_SIZE")
             .parse::<u32>()
             .expect("POOL_SIZE shoudl be number"),
+    }
+}
+
+pub fn rabbit_mq_settings() -> RabbitMQSettings {
+    RabbitMQSettings {
+        url: get_env("RABBIT_MQ_URL"),
+        queue: get_env("QUEUE_NAME"),
+        consumer_tag: get_env("CONSUMER_TAG"),
     }
 }
 
